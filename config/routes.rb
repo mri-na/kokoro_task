b2724006c4aef93806e58601b5a80c1bb45648b1
mri-na <marina.89260106@gmail.com>
2024-12-23 22:14:33 +0900

    ダッシュボードの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
   devise_for :users
-  root to: 'journals#index'
+  get 'dashboard', to: 'dashboard#index'
+  root to: 'dashboard#index'
   resources :journals do
     resources :mood_options, only: [:new, :create]
   end
