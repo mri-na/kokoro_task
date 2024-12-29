01adfa24cff1ae57bca31a2c6a7a63c2c5693667
mri-na <marina.89260106@gmail.com>
2024-12-29 12:59:36 +0900

    タスク詳細画面

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,9 +2,17 @@ class Task < ApplicationRecord
   belongs_to :user
   has_one :mood_option, as: :optionable, dependent: :destroy
   validates :title, presence: true
-  validates :status, inclusion: { in: ['not_started', 'in_progress', 'completed'] }
-  validates :priority, inclusion: { in: ['low', 'medium', 'high'] }
+  validates :status, inclusion: { in: ['未着手', '進行中', '完了'] }
+  validates :priority, inclusion: { in: ['優先度３', '優先度２', '優先度１'] }
 
   enum status: { not_started: 0, in_progress: 1, completed: 2 }
   enum priority: { low: 0, medium: 1, high: 2 }
+
+  def status_in_japanese
+    I18n.t("activerecord.attributes.task.status.#{status}")
+  end
+
+  def priority_in_japanese
+    I18n.t("activerecord.attributes.task.priority.#{priority}")
+  end
 end
